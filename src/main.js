04d0a888f368ccb8d4626ab501f8bff7b205c237
SANGWON LEE <yonga3749@naver.com>
2023-08-02 15:38:28 +0900

    JS - StopGame

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@
 const MINION_SIZE = 130;
 const MINION_COUNT = 6;
 const EVIL_COUNT = 3;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer__left');
 const gameScore = document.querySelector('.game__score__total');
+
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
 
 let started = false;
 let score = 0;
@@ -27,9 +32,14 @@ function startGame() {
   initGame();
   showStopButton();
   showTimeAndScore();
+  startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText('Try Again💡');
+}
 
 function showStopButton() {
   const icon = gameBtn.querySelector('.fa-google-play');
@@ -37,9 +47,40 @@ function showStopButton() {
   icon.classList.remove('fa-google-play');
 }
 
+function hideGameButton() {
+  gameBtn.style.visibility = 'hidden';
+}
+
 function showTimeAndScore() {
   gameTimer.style.visibility = 'visible';
   gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes} : ${seconds}`;
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove('pop-up--hide');
 }
 
 function initGame() {
